9608887425a3b5e697b95b4014d69df032d68c51
ouyangmland <ouyangmland@gmail.com>
2025-01-07 17:02:45 +0800

    Update appex.sh

--- appex.sh
+++ appex.sh
@@ -107,7 +107,7 @@ mkdir -p /appex/bin
 MAC=$(ifconfig "$Eth" |awk '/HWaddr/{ print $5 }')
 [ -z "$MAC" ] && MAC=$(ifconfig "$Eth" |awk '/ether/{ print $2 }')
 [ -z "$MAC" ] && Uninstall && echo "Not Found MAC address! " && exit 1
-wget --no-check-certificate -q -O "/appex/etc/apx.lic" "http://speedsvip.000webhostapp.com/apx.php?mac=$MAC"
+wget --no-check-certificate -q -O "/appex/etc/apx.lic" "http://rs.91yun.pw/apx1.php?mac=$MAC"
 [ "$(du -b /appex/etc/apx.lic |awk '{ print $1 }')" -ne '152' ] && Uninstall && echo "Error! I can not generate the Lic for you, Please try again later! " && exit 1
 echo "Lic generate success! "
 [ -n $(which ethtool) ] && rm -rf /appex/bin/ethtool && cp -f $(which ethtool) /appex/bin
